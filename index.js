51d7d8c1046cfa64728f763257dd8ca05d69479b
Kavya-JH <hkavyaj@gmail.com>
2024-05-29 21:33:08 +0530

    complete jokeapi

--- index.js
+++ index.js
@@ -22,8 +22,9 @@ app.post ("/", async (req, res) => {
     try{
     const response = await axios.get(`https://v2.jokeapi.dev/joke/Any?type=single&idRange=${lastNameChar}-${totalchar}&amount=1`);
     const result = response.data;
+    const randomgif = Math.floor(Math.random()*2);
     console.log(result);
-    res.render("index.ejs", {data : result});
+    res.render("index.ejs", {data : result, giff : randomgif });
     }
     catch(error){
         res.statusCode(404).render("index.ejs");
